083024694d9312742f4c072efb2494edec4383b9
Ahmed Sayed Mohamed <ahmed.sayed.ashourr@gmail.com>
2024-07-27 13:45:36 +0300

    * Finish Radio Ui

--- lib/modules/Radio_View/radioView.dart
+++ lib/modules/Radio_View/radioView.dart
@@ -5,14 +5,31 @@ class radioView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Radio_View",
-        style: TextStyle(
-            fontFamily: "El Messeri",
-            fontSize: 15,
-            fontWeight: FontWeight.w700),
-      ),
-    );
+    var theme = Theme.of(context);
+    return
+      Center(
+        child: Column(
+        children: [
+          SizedBox(height:80,),
+          Image.asset("assets/icons/551-5517026_radio-vector-png-old-radio-png-vector-transparent.png"),
+          SizedBox(height:40,),
+          Text("إذاعة القرآن الكريم",style:theme.textTheme.bodyMedium,),
+          SizedBox(height:40,),
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.skip_previous,color:theme.primaryColor,size:40,),
+                SizedBox(width:40,),
+                Icon(Icons.play_arrow_outlined,color:theme.primaryColor,size:40,),
+                SizedBox(width:40,),
+                Icon(Icons.skip_next,color:theme.primaryColor,size:40,),
+              ],
+            ),
+          )
+          
+        ],
+            ),
+      );
   }
 }
